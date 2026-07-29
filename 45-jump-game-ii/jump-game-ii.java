073be1daf9d535a7cfa36bc79a07e2d649360e80
Nayan Gawatre <142825739+nayangawatre@users.noreply.github.com>
2026-07-29 15:58:14 +0530

    Time: 2 ms (30.04%) | Memory: 47.7 MB (8.61%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int a=0,b=0;
+        int c=0;
+        for(int i=0;i<nums.length-1 ;i++ ){
+            a=Math.max(a,i+nums[i]);
+            
+            if(b==i){
+                c++;
+                b=a;
+            }
+        }
+        return c;
+        
+    }
+}
